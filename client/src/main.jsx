b71f29124baf79883f57aa143466bb558e749d47
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2024-01-17 22:39:14 +0530

    Updated main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {Toaster} from "react-hot-toast"
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Toaster className="toast" position="bottom-left"/>
